d119080df69b11ce7d8cc86877cea1ac35596b70
Philip Hagen <phil@philiphagen.com>
2015-11-26 16:26:30 +1100

    Implement the AnagramFinder class

--- lib/anagram_finder.rb
+++ lib/anagram_finder.rb
@@ -1,10 +1,21 @@
 class AnagramFinder
 
+  def initialize
+    @store = Hash.new { |h, k| h[k] = [] }
+  end
+
   def add_word(word)
+    @store[checksum(word)] << word
   end
 
   def anagrams
-    [["cat", "tac"]]
+    @store.select {|checksum, words| words.length > 1 }.values
+  end
+
+  private
+
+  def checksum(word)
+    word.chars.sort.join.hash
   end
 
 end
